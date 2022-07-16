d1e9d7abbd899f448f8dec9d75313223b2caa4ba
Davidenko Ilia <Ilia_d@outlook.com>
2022-07-16 13:23:13 +0300

    task 1

---
+++ 1.py
@@ -0,0 +1,19 @@
+# Задайте список из нескольких чисел. Напишите программу, 
+# которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+# *Пример:*
+
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+
+lst = [2,5,7,3,8,4,5,4]
+
+def sum_negative_index(lst):
+    lenght_lst = len(lst)
+    sum = 0
+    for i in range(lenght_lst):
+        if i % 2 == 0:
+            False
+        else:
+            sum += lst[i]
+    return sum
+print(sum_negative_index(lst))
