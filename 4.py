104d11c25704a64812278cf30b5c8282da870f4d
Davidenko Ilia <Ilia_d@outlook.com>
2022-07-17 13:33:58 +0300

    task 4

---
+++ 4.py
@@ -0,0 +1,19 @@
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# *Пример:*
+
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+dec_number = int(input('введите число '))
+
+def bi(dec_number):
+    bi_number = ''
+    while True:
+        bi_number = bi_number + str(dec_number % 2)
+        dec_number = dec_number // 2
+        if (dec_number == 0 or dec_number == 1):
+            bi_number = bi_number + str(dec_number)
+            break
+    return bi_number[::-1]
+print(f'десятичное число {dec_number} в двоичной системе = {bi(dec_number)}')
